7dd860fd00d42b5d17074378614e9e63b1e3e7b1
Tobajoseph007 <toba@darey.io>
2022-01-22 16:10:09 +0000

    implementing environment variable

--- html/db_conn.php
+++ html/db_conn.php
@@ -1,16 +1,31 @@
 <?php
 
-// enabling environmental variable for the parameters 
-$servername = "localhost" ;
-$username = '.$_ENV["MYSQL_USERNAME"]' ;
-$password = '.$_ENV["MYSQL_PW"]' ;
-$dbname = '.$_ENV["MYSQL_DBNAME"]' ;
+
+//connect to .env 
+
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+//loads the required env when called
+$dotenv->load();  
+
+//get database connectionfrom environment variable file
+
+
+$Hostname = $_ENV["MYSQLIP"]; // input servername
+$dbUsername = $_ENV["MYSQLUSER"]; // input username
+$dbPassword = $_ENV["MYSQLPASS"]; //input password
+$dbName = $_ENV["MYSQLDBNAME"]; // input dbname
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($Hostname, $dbUsername, $dbPassword, $dbName);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+
+
 ?>
